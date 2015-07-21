bd64c8277195de70ee1779687346675c472ed9de
Tatiana Didik <aquarellian@gmail.com>
2015-07-21 15:22:34 -0400

    Initial jenkins settings

---
+++ src/main/java/com/aquarellian/genar/jenkins/HelloWorldBuilder.java
@@ -0,0 +1,11 @@
+package com.aquarellian.genar.jenkins;
+
+/**
+ * Project: genar
+ * Author:  Tatiana Didik
+ * Created: 21.07.2015 12:46
+ * <p/>
+ * $Id$
+ */
+public class HelloWorldBuilder {
+}
