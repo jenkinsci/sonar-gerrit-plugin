5c08f41c4f41ae0a4c8af8ee9cdca488607465a7
Tatiana Didik <aquarellian@gmail.com>
2015-09-04 18:28:36 -0400

    plugin executable file
    Former-commit-id: ee30f13d2e59a13a61e54fd9031a89b99ccadd63

---
+++ src/main/java/com/aquarellian/genar/data/converter/BasicIssueFormatter.java
@@ -0,0 +1,11 @@
+package com.aquarellian.genar.data.converter;
+
+/**
+ * Project: genar
+ * Author:  Tatiana Didik
+ * Created: 03.09.2015 19:39
+ * <p/>
+ * $Id$
+ */
+public class BasicIssueFormatter {
+}
---
+++ src/main/java/com/aquarellian/genar/data/converter/IssueFormatter.java
@@ -0,0 +1,11 @@
+package com.aquarellian.genar.data.converter;
+
+/**
+ * Project: genar
+ * Author:  Tatiana Didik
+ * Created: 03.09.2015 19:38
+ * <p/>
+ * $Id$
+ */
+public interface IssueFormatter {
+}
