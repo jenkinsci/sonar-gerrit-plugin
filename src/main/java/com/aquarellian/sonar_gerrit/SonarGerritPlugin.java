44b14a00064407c19a0c764c31da32013c9ad9eb
Tatiana Didik <aquarellian@gmail.com>
2015-09-16 22:19:29 -0400

    add appropriate logging

---
+++ src/main/java/com/aquarellian/sonar_gerrit/SonarGerritPlugin.java
@@ -0,0 +1,20 @@
+package com.aquarellian.sonar_gerrit;
+
+import hudson.Plugin;
+
+import java.util.logging.Logger;
+
+/**
+ * Project: Sonar-Gerrit Plugin
+ * Author:  Tatiana Didik
+ * Created: 16.09.2015 21:27
+ * <p/>
+ * $Id$
+ */
+public class SonarGerritPlugin extends Plugin {
+    private final static Logger LOG = Logger.getLogger(SonarGerritPlugin.class.getName());
+
+    public void start() throws Exception {
+        LOG.info("SonarGerrit: sending SonarQube results to Gerrit");
+    }
+}
