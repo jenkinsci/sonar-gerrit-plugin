1f4b9e7fae0af31ee6060b287f1a7bcee4eb46c4
Tatiana Didik <aquarellian@gmail.com>
2015-12-03 20:59:53 -0500

    JENKINS-31892 Support multiple project locations for multi jobs

---
+++ src/main/java/org/jenkinsci/plugins/sonargerrit/SubJobConfig.java
@@ -0,0 +1,11 @@
+package org.jenkinsci.plugins.sonargerrit;
+
+/**
+ * Project: Sonar-Gerrit Plugin
+ * Author:  Tatiana Didik
+ * Created: 02.12.2015 12:11
+ * <p/>
+ * $Id$
+ */
+public class SubJobConfig {
+}
