67dc39df12987fe454fe8f72d01558c0a0775864
Réda Housni Alaoui <reda-alaoui@hey.com>
2022-07-04 18:35:52 +0200

    Allow to exclude score and review issues by glob pattern

--- src/main/java/org/jenkinsci/plugins/sonargerrit/sonar/ByGlobPatternPredicate.java
+++ src/main/java/org/jenkinsci/plugins/sonargerrit/sonar/ByGlobPatternPredicate.java
@@ -10,14 +10,33 @@ import org.apache.commons.lang3.StringUtils;
 public class ByGlobPatternPredicate implements Predicate<Issue> {
 
   private final PathMatcher pathMatcher;
+  private final boolean negated;
 
   public ByGlobPatternPredicate(String pattern) {
-    this.pathMatcher = FileSystems.getDefault().getPathMatcher("glob:" + pattern);
+    this(pattern, false);
+  }
+
+  public ByGlobPatternPredicate(String pattern, boolean negated) {
+    this(FileSystems.getDefault().getPathMatcher("glob:" + pattern), negated);
+  }
+
+  public ByGlobPatternPredicate(PathMatcher pathMatcher, boolean negated) {
+    this.pathMatcher = pathMatcher;
+    this.negated = negated;
+  }
+
+  public ByGlobPatternPredicate negate() {
+    return new ByGlobPatternPredicate(pathMatcher, !negated);
   }
 
   @Override
   public boolean apply(Issue input) {
     String filepath = StringUtils.prependIfMissing(input.getFilepath(), "/");
-    return pathMatcher.matches(Paths.get(filepath));
+    boolean matched = pathMatcher.matches(Paths.get(filepath));
+    if (negated) {
+      return !matched;
+    } else {
+      return matched;
+    }
   }
 }
